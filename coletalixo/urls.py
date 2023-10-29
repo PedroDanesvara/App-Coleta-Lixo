931f175ceb804cb1919754000d70e2ebbafa5980
dericcarvalho <dericcarvalho.silva@gmail.com>
2023-10-29 16:45:32 -0300

    urls

--- coletalixo/urls.py
+++ coletalixo/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.oferta.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('ofertas/', include('apps.oferta.urls')),
+    path('obtain-token/', obtain_auth_token),
+    path("", include("apps.usuario.urls")),
 ]
